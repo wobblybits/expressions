892b0ab8e82b0bf1484befb1c4f17202f8ce3308
wobblybits <somebody@wobblybits.blog>
2025-07-30 02:57:06 -0500

    TSP MONKEY

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Actor from './emotions/Actor';
 import Face from './emotions/Face';
 import Game from './emotions/Game';
 import { For } from 'solid-js';
+import ImageTSP from './emotions/ImageTSP';
 
 const App: Component = () => {
   const emotionModel = new EmotionModel();
@@ -66,7 +67,8 @@ const App: Component = () => {
         <Actor id="actor" width={size} height={size} expressionModel={expressionModel} emotionLevels={randomExpression()}/>
         <Actor id="actor" width={size} height={size} expressionModel={expressionModel} emotionLevels={randomExpression()}/>
       </div> */}
-      <Game id="game" width={Math.min(size*gridSize, window.innerWidth * .95, window.innerHeight * .95)} height={Math.min(size*gridSize, window.innerWidth * .95, window.innerHeight * .95)} expressionModel={expressionModel} gridSize={gridSize}/>
+      {/* <Game id="game" width={Math.min(size*gridSize, window.innerWidth * .95, window.innerHeight * .95)} height={Math.min(size*gridSize, window.innerWidth * .95, window.innerHeight * .95)} expressionModel={expressionModel} gridSize={gridSize}/> */}
+      <ImageTSP emotionModel={emotionModel} emotionLevels={randomExpression()}/>
     </div>
   );
 };
