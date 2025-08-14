1351b9f883b63e972c30925e1040e9d8f3303ec5
wobblybits <somebody@wobblybits.blog>
2025-08-14 08:14:08 -0500

    Cleanup from reorg

--- ellipses/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@solidjs/start/config";
-
-export default defineConfig({
-  ssr: false,
-  server: process.env.NODE_ENV === "production" ? {
-    preset: "static",
-    baseURL: "/pareidolia/", // GitHub Pages subdirectory
-  } : undefined,
-}); 
