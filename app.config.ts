206266341f00e038fbbd3508b71839d47fc3d685
wobblybits <somebody@wobblybits.blog>
2025-08-14 08:27:20 -0500

    Base path config

--- app.config.ts
+++ app.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   ssr: false,//process.env.NODE_ENV === "production",
   server: process.env.NODE_ENV === "production" ? {
     preset: "static",
+    baseURL: "/pareidolia/",
     prerender: {
       routes: ["/", "/about", "/game", "/composite", "/pareidolia"],
       crawlLinks: false
