8fbbf72a604e99dfb2ca5df8091f94e1c82f055d
wobblybits <somebody@wobblybits.blog>
2025-08-18 08:15:32 -0500

    Fixed tps conversion to typescript

--- src/tps/TPS.ts
+++ src/tps/TPS.ts
@@ -37,19 +37,7 @@ export default class TPS extends Transformation {
    * @param point - array of coordinates - [Northing, Easting]
    */
   forward(point: number[]): number[] {
-    const parameters = this.forwardParameters as TPSParameters;
-    if (!parameters) {
-      return point;
-    }
-
-    let Xo = parameters.Xc[0] + parameters.Xc[1] * point[0] + parameters.Xc[2] * point[1],
-      Yo = parameters.Yc[0] + parameters.Yc[1] * point[0] + parameters.Yc[2] * point[1];
-    for (let r = 0; r < parameters.m; r++) {
-      const tmp = this.__kernelFunction(point[0] - parameters.sourcePoints[r][0], point[1] - parameters.sourcePoints[r][1]);
-      Xo += parameters.Xc[r + 3] * tmp;
-      Yo += parameters.Yc[r + 3] * tmp;
-    }
-    return [Xo, Yo];
+    return this.__isEmpty(this._forwardParameters) ? point : this.__transform(point, this._forwardParameters as TPSParameters);
   }
 
   /**
@@ -57,11 +45,15 @@ export default class TPS extends Transformation {
    * @param point - array of coordinates - [Northing, Easting]
    */
   inverse(point: number[]): number[] {
-    const parameters = this.inverseParameters as TPSParameters;
-    if (!parameters) {
-      return point;
-    }
+    return this.__isEmpty(this._inverseParameters) ? point : this.__transform(point, this._inverseParameters as TPSParameters);
+  }
 
+  /**
+   * Transforms a point
+   * @param point - array of coordinates - [Northing, Easting]
+   * @param parameters - TPS transformation parameters
+   */
+  private __transform(point: number[], parameters: TPSParameters): number[] {
     let Xo = parameters.Xc[0] + parameters.Xc[1] * point[0] + parameters.Xc[2] * point[1],
       Yo = parameters.Yc[0] + parameters.Yc[1] * point[0] + parameters.Yc[2] * point[1];
     for (let r = 0; r < parameters.m; r++) {
@@ -98,42 +90,69 @@ export default class TPS extends Transformation {
       A[3 + i][0] = 1;
       A[3 + i][1] = sourcePoints[i][0];
       A[3 + i][2] = sourcePoints[i][1];
+    }
 
-      // the kernel function for each respective point combination
-      for (let j = 0; j < m; j++) {
-        A[3 + i][3 + j] = this.__kernelFunction(sourcePoints[i][0] - sourcePoints[j][0], sourcePoints[i][1] - sourcePoints[j][1]);
+    // the lower right part of the matrix
+    for (let r = 0; r < m; r++) {
+      for (let c = 0; c < m; c++) {
+        A[r + 3][c + 3] = this.__kernelFunction(sourcePoints[r][0] - sourcePoints[c][0], sourcePoints[r][1] - sourcePoints[c][1]);
+        A[c + 3][r + 3] = A[r + 3][c + 3];
       }
     }
 
-    // we'll try to get the inverse
     this.invA = invertMatrix(A);
 
-    if (!this.invA) {
-      console.warn('Matrix could not be inverted!');
-      return null;
-    }
+    if (this.invA === null) return null;
 
-    // prepare the X coordinate array
-    const Xc = new Float64Array(m + 3);
-    // prepare the Y coordinate array
-    const Yc = new Float64Array(m + 3);
-
-    // calculate Y
-    for (let i = 0; i < m + 3; i++) {
-      Xc[i] = 0;
-      Yc[i] = 0;
-      for (let j = 0; j < m; j++) {
-        Xc[i] += this.invA[i][3 + j] * targetPoints[j][0];
-        Yc[i] += this.invA[i][3 + j] * targetPoints[j][1];
+    let Xc = new Float64Array(m + 3),
+    Yc = new Float64Array(m + 3);
+    for (let r = 0; r < m + 3; r++) {
+      for (let c = 0; c < m; c++) {
+        Xc[r] += this.invA[r][c + 3] * targetPoints[c][0];
+        Yc[r] += this.invA[r][c + 3] * targetPoints[c][1];
       }
     }
 
-    return {
-      m,
-      Xc,
-      Yc,
-      sourcePoints
-    };
+    return { m, Xc, Yc, sourcePoints };
+  }
+
+  /**
+   * Set forward transformation parameters
+   * @param targetPoints - array of points - [Northing, Easting]
+   */
+  updateParameters(targetPoints: number[][]): { Xc: Float64Array; Yc: Float64Array } {    
+    this._targetPoints = targetPoints;
+    const m = this.targetPoints.length;
+    let Xc = new Float64Array(m + 3),
+    Yc = new Float64Array(m + 3);
+    for (let r = 0; r < m + 3; r++) {
+      for (let c = 0; c < m; c++) {
+        Xc[r] += this.invA![r][c + 3] * targetPoints[c][0];
+        Yc[r] += this.invA![r][c + 3] * targetPoints[c][1];
+      }
+    }
+    this._forwardParameters.Xc = [...Xc];
+    this._forwardParameters.Yc = [...Yc];
+    return { Xc, Yc };
+  }
+
+  /**
+   * Set inverse transformation parameters
+   * @param targetPoints - array of points - [Northing, Easting]
+   */
+  updateInverseParameters(targetPoints: number[][]): TPSParameters {
+    this._targetPoints = [...targetPoints];
+    const newParams = this.__calculateParameters(this._targetPoints, this._sourcePoints);
+    if (newParams && !this.__isEmpty(newParams)) {
+      this._inverseParameters = newParams;
+      // this._inverseParameters.Yc[0] = 0;
+      this._inverseParameters.Yc[1] = 0;
+      this._inverseParameters.Yc[2] = 1;
+      // this._inverseParameters.Xc[0] = 0;
+      this._inverseParameters.Xc[1] = 1;
+      this._inverseParameters.Xc[2] = 0;
+    }
+    return this._inverseParameters as TPSParameters;
   }
 
   /**
@@ -142,21 +161,17 @@ export default class TPS extends Transformation {
    * @param dy - difference in y
    */
   private __kernelFunction(dx: number, dy: number): number {
-    const cacheKey = `${dx.toFixed(6)},${dy.toFixed(6)}`;
-    
-    if (this.kernelCache.has(cacheKey)) {
-      return this.kernelCache.get(cacheKey)!;
-    }
-
-    let result: number;
-    if (dx === 0 && dy === 0) {
-      result = 0;
-    } else {
-      const dist = Math.sqrt(dx * dx + dy * dy);
-      result = dist * dist * Math.log(dist * dist);
-    }
-
-    this.kernelCache.set(cacheKey, result);
+    if (dx == 0 && dy == 0) return 0;
+    const dist = dx * dx + dy * dy;
+    const result = dist * Math.log(dist);
     return result;
+  }
+
+  /**
+   * Checks if an object has any properties
+   * @param obj - object to check
+   */
+  private __isEmpty(obj: Record<string, any>): boolean {
+    return Object.keys(obj).length === 0 && obj.constructor === Object;
   }
 } 
